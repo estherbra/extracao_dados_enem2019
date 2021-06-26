b3b8e20e7b9efab91b981e309b2cb0546fb77c33
Esther Brasileiro <brasileiroesther@gmail.com>
2021-06-26 01:28:02 -0300

    primeiro commit

---
+++ extracao_enem.py
@@ -0,0 +1,28 @@
+##Extração de dados do Enem 2019
+#Importar bibliotecas necessárias
+import pandas as pd
+import numpy as np
+import zipfile
+import requests
+from io import BytesIO
+import os
+
+#Criar diretórios para armazenamento do conteúdo
+os.makedirs('./enem2019', exist_ok=True)
+
+#Define a URL
+url= "https://download.inep.gov.br/microdados/microdados_enem_2019.zip"
+
+#Faz download do conteúdo
+filebytes = BytesIO(
+    requests.get(url).content
+)
+
+#Extrai o conteúdo do arquivo zip
+myzip = zipfile.ZipFile(filebytes)
+myzip.extractall('./enade2019')
+
+##Verifica os itens baixados
+enem = pd.read_csv(
+    './enem2019/DADOS/MICRODADOS_ENEM_2019.csv' , engine = 'python' , sep = ';' , decimal = ','
+)
